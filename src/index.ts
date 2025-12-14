29b803450ab22b0dc0b554fae976e39333848a38
Rustam Mullaev <sportandyoung@gmail.com>
2025-12-14 11:20:55 +0500

    fix port

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express from "express"
 import type { Request, Response} from "express";
+import cors from "cors"
+import bodyParser from "body-parser"
+
+
 
 const app = express()
-const port = 3000
+
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+const jsonBodyMidlleware = bodyParser.json()
+
+const port = process.env.PORT || 5000
 
 app.get('/', (req: Request, res: Response) => {
   let helloMessage = 'Hello mister Rustik!'
